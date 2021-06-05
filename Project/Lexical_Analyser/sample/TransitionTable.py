ed1ba4aba2d2d59e530cd7f147a0f552e53bd12c
holondo <holondo@outlook.com>
2021-06-05 16:25:37 -0300

    transition table class

---
+++ Project/Lexical_Analyser/sample/TransitionTable.py
@@ -0,0 +1,26 @@
+import csv
+
+class TransitionTable:
+
+    fileName = "TTable.csv"
+    state = [] #List of dictionary shaped states
+
+    def __init__(self):        
+        stateTransitions = {}
+
+        with open(self.fileName, 'r') as fp:
+            CSVReader = csv.reader(fp)
+            lines = list(CSVReader)
+
+            for i in range(1, len(lines)): #For each line in csv, make a dictionary
+                for j in range(len(lines[0])):
+                    stateTransitions[lines[0][j]] = lines[i][j]
+
+                self.state.append(stateTransitions.copy())
+                stateTransitions.clear();
+    
+    def transition(self, currentState, symbol ):
+        toReturn = self.state[currentState][symbol]
+        return toReturn
+
+
