50fc17626a42cc3b0811f109283b5001bf09e86a
viniciusbaca <viniciusfbaca@usp.br>
2021-06-06 12:14:48 -0300

    feat: symbols table

--- Project/Lexical_Analyser/sample/tester.py
+++ Project/Lexical_Analyser/sample/tester.py
@@ -1,4 +1,9 @@
 from TransitionTable import TransitionTable
 
+x = TransitionTable('symbols.csv')
 y = TransitionTable('TTable.csv')
 print(y.transition(0, '>'))
+print(y.transition(2, '<'))
+print(y.transition(3, 'alpha'))
+
+print(x.transition(0, 'Retorno'))
