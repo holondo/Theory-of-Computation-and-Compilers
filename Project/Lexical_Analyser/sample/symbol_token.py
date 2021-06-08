cd50d7261e1f084c5a18f34990f6daa31d3052de
holondo <holondo@outlook.com>
2021-06-07 23:14:55 -0300

    creation of the Table of symbols

--- Project/Lexical_Analyser/sample/symbol_token.py
+++ Project/Lexical_Analyser/sample/symbol_token.py
@@ -1,3 +1,14 @@
-class symbol:
-    str = ""
-    token = ""
+import pandas as pd
+
+class TableOfSymbols:
+    fileName = "symbols.csv"
+    def __init__(self):
+        self.table = pd.read_csv(self.fileName, index_col = 'Nome')
+
+    def inTableOfSymbols(self, strToVerify):
+        try:            
+            toReturn = self.table.loc[strToVerify]['Retorno']
+        except:
+            toReturn = ""
+        
+        return toReturn
