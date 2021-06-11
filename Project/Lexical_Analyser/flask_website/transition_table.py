af6ec9b38f005367c3ea941c40d08640ba4782d1
holondo <holondo@outlook.com>
2021-06-11 18:36:16 -0300

    Rearranged files

--- Project/Lexical_Analyser/flask_website/transition_table.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd 
-
-class TransitionTable:
-    def __init__(self,csvFile):
-        self.df = pd.read_csv(csvFile)
-
-    def transition(self,currentState, symbol):
-        match = self.df.iloc[currentState][symbol]
-        return match
