6fa10fe2c40095fc60abb808a8e840776c9c207d
holondo <holondo@outlook.com>
2021-06-13 21:48:51 -0300

    now working with files

--- Project/Lexical_Analyser/app.py
+++ Project/Lexical_Analyser/app.py
@@ -1,13 +1,24 @@
-from flask import Flask, render_template, request
+import os
+from flask import Flask, render_template, request, redirect
 from sample.lexerDeliver import sourceToLexer
 
+UPLOAD_FOLDER = "./uploads"
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = os.path.join(os.path.dirname(__file__), UPLOAD_FOLDER)
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     #In the homepage, if the button send a POST method
     if(request.method == "POST"):
-        show = sourceToLexer(request.form["txtProgram"]) #Sends txtProgram's content to the "Parser" who returns Lexer's work 
+        file = request.files['file']
+        if file.filename == '':
+            return redirect(request.url)
+
+        filePath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(filePath)        
+        file = open(filePath, 'r')
+        show = sourceToLexer(file.read()) #Sends txtProgram's content to the "Parser" who returns Lexer's work
+
         return render_template("home2.html", boolResult=True, strProgram=show)
 
     #If the app receives a GET method
