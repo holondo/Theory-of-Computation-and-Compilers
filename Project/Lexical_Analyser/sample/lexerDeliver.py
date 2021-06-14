f739d6badffb8993f4eef4ad3ff033e41ab5923e
holondo <holondo@outlook.com>
2021-06-14 17:55:50 -0300

    bugs fixed

--- Project/Lexical_Analyser/sample/lexerDeliver.py
+++ Project/Lexical_Analyser/sample/lexerDeliver.py
@@ -3,15 +3,10 @@ from .lexer import Lexer
 def sourceToLexer(strProgram):
     show = []
     lex = Lexer()
-
-    strProgram = strProgram.split()
-
-    for section in strProgram:
-        section += ' ' #end symbol
-
-        while section != "":
-            strToken = lex.lexicalAnalyzer(section)
-            section = section[len(strToken[0]):]#slicing the already processed part of string
-            if strToken[1] == 'BLANK_SPACE': continue
-            show.append(strToken)
+    strProgram += " " #final character
+    while strProgram != "":
+        strToken = lex.lexicalAnalyzer(strProgram)
+        strProgram = strProgram[len(strToken[0]):]#slicing the already processed part of string
+        if strToken[1] == 'BLANK_SPACE': continue
+        show.append(strToken)
     return show
