bc5ccbfd2f5956a1c87fc3eaf18937092553dba9
holondo <holondo@outlook.com>
2021-07-15 18:53:06 -0300

    some Declarations working

--- Project/Lexical_Analyser/sample/SyntacticAnalyser.py
+++ Project/Lexical_Analyser/sample/SyntacticAnalyser.py
@@ -1,34 +1,211 @@
+from numpy import equal
 from .lexer import Lexer
+
+FILE_END = " "
 
 class SyntacticAnalyser:
     symb = None
     def __init__(self):
         self.currentLine = 1;
         self.__originalProgram__ = ""
+        self.errors = list()
         self.strProgram = ""
         self.lexer = Lexer()
-        self.errors = list()
     
-    def loadNextSymbol(self):
-        self.symb = self.lexer.lexicalAnalyzer(self.strProgram)# = (symbol,token)
-        self.strProgram = self.strProgram[len(self.symb[0]):]#slicing the already processed part of string
+    def programIsProcessed(self) -> bool:
+        if self.strProgram == "": 
+            return True
+        return False
 
-        if self.symb[1] == 'BLANK_SPACE':
-            if self.symb[0] == "\n":
-                self.currentLine += 1
-            self.loadNextSymbol()
+    def getCurrentToken(self) -> str:
+        return self.symb[1]
     
-    def analyse(self,strProgram):
+    def getCurrentSymbol(self) -> str:
+        return self.symb[0]
+    
+    ### Returns: true, if 
+    def isCurrentToken(self, expectedToken) -> bool:
+        return (self.getCurrentToken() == expectedToken)
+
+    def loadNextSymbol(self):
+        if not self.programIsProcessed():
+            self.symb = self.lexer.lexicalAnalyzer(self.strProgram)# = (symbol,token)
+            self.strProgram = self.strProgram[len(self.symb[0]):]#slicing the already processed part of string
+
+            if self.getCurrentToken() == 'BLANK_SPACE':
+                if '\n' in self.symb[0]:
+                    self.currentLine += 1
+                self.loadNextSymbol()
+        
+    def analyse(self,strProgram) -> list:
         if not isinstance(strProgram, str):
             self.errors.append("Arquivo com problemas.\n")
             return 
 
         self.__originalProgram__ = strProgram
         self.strProgram = strProgram
+        self.strProgram += FILE_END
 
         self.loadNextSymbol()
 
-        self.program_production()#primeira regra de producao
+        self.prod_Program()#primeira regra de producao
 
-        if self.strProgram == "" and len(self.errors) == 0:
+        if self.strProgram == FILE_END and len(self.errors) == 0:
             self.errors.append("Compilação terminada com sucesso")
+        
+        return self.errors
+
+    
+    def error(self, errorMessage, currentNext, productionNext) -> bool:
+        if self.isCurrentToken("INCOMPLETE_FLOAT_ERROR"):
+                self.errors.append(f"Erro léxico na linha {self.currentLine}: {self.symb}.")
+        
+        self.errors.append(f"Erro sintático na linha {self.currentLine}: {errorMessage}")
+
+        if self.getCurrentToken() in currentNext:
+            return True
+        
+        if self.getCurrentToken() in productionNext:
+            return False
+
+        while not self.programIsProcessed() and self.getCurrentToken() not in productionNext:
+            if productionNext == FILE_END and self.getCurrentSymbol() == FILE_END:
+                return False
+            self.loadNextSymbol()
+        
+        return False
+
+
+    #|/|/|/|/|/|/| Production Rulez
+
+    def prod_Program(self):
+        if self.isCurrentToken("PROGRAM_SYMB"):
+            self.loadNextSymbol()
+        else:
+            if self.error("Program esperado.", currentNext= "IDENTIFIER", productionNext= FILE_END) == False:
+                return
+        
+        if self.isCurrentToken("IDENTIFIER"):
+            self.loadNextSymbol()
+        else:
+            if self.error("Identificador do programa esperado.", currentNext= "SEMICOLON_SYMB", productionNext= FILE_END) == False:
+                return
+            
+        if self.isCurrentToken("SEMICOLON_SYMB"):
+            self.loadNextSymbol()
+        else:
+            if self.error("; esperado.", currentNext= ["CONST_SYMB", "VAR_SYMB", "PROCEDURE_SYMB"], productionNext= FILE_END) == False:
+                return
+
+        self.prod_Body()
+
+        if self.isCurrentToken("DOT_SYMB"):
+            self.loadNextSymbol()
+        else:
+            if self.error("Ponto final esperado.", currentNext= FILE_END, productionNext= FILE_END) == False:
+                return
+    
+    def prod_Body(self):
+
+        #-----DECLARATIONS
+        self.prod_dc_c()
+        while self.isCurrentToken("CONST_SYMB"):
+            self.prod_dc_c()
+            
+        self.prod_dc_v()
+        while self.isCurrentToken("VAR_SYMB"):
+            self.prod_dc_v()
+
+        self.prod_dc_p()
+        while self.isCurrentToken("PROCEDURE_SYMB"):
+            self.prod_dc_v()
+        #-----DECLARATIONS END
+
+        if self.isCurrentToken("BEGIN_SYMB"):
+            self.loadNextSymbol()
+        else:
+            if self.error("begin esperado.", currentNext= ["WRITE_SYMB", "WHILE_SYMB", "IF_SYMB", "IDENTIFIER", "BEGIN","END_SYMB"], productionNext= "DOT_SYMB") == False:
+                return
+
+        #####TODO <COMANDOS>
+
+        if self.isCurrentToken("END_SYMB"):
+            self.loadNextSymbol()
+        else:
+            if self.error("End esperado.", currentNext= "DOT_SYMB", productionNext= "DOT_SYMB") == False:
+                return
+        
+    def prod_dc_c(self):
+        if self.isCurrentToken("CONST_SYMB"):
+            self.loadNextSymbol()
+        else:
+            if self.error("const esperado.", currentNext= "IDENTIFIER", productionNext= ["CONST_SYMB","VAR_SYMB", "PROCEDURE_SYMB", "BEGIN_SYMB"]) == False:
+                self.errors.pop()
+                return
+
+        if self.isCurrentToken("IDENTIFIER"):
+            self.loadNextSymbol()
+        else:
+            if self.error("Identificador de constante esperado.", currentNext= "EQUAL_SIGN", productionNext= ["CONST_SYMB","VAR_SYMB", "PROCEDURE_SYMB", "BEGIN_SYMB"]) == False:
+                return
+
+        if self.isCurrentToken("EQUAL_SIGN"):
+            self.loadNextSymbol()
+        else:
+            if self.error("Simbolo '=' esperado.", currentNext= ["UNSIGNED_INTEGER", "UNSIGNED_FLOAT"], productionNext= ["CONST_SYMB","VAR_SYMB", "PROCEDURE_SYMB", "BEGIN_SYMB"]) == False:
+                return
+        
+        if self.isCurrentToken("UNSIGNED_INTEGER") or self.isCurrentToken("UNSIGNED_FLOAT"):
+            self.loadNextSymbol()
+        else:
+            if self.error("Valor numérico esperado.", currentNext= "SEMICOLON_SYMB", productionNext= ["CONST_SYMB","VAR_SYMB", "PROCEDURE_SYMB", "BEGIN_SYMB"]) == False:
+                return
+
+        if self.isCurrentToken("SEMICOLON_SYMB"):
+            self.loadNextSymbol()
+        else:
+            if self.error("; esperado.", currentNext= ["CONST_SYMB","VAR_SYMB", "PROCEDURE_SYMB", "BEGIN_SYMB"], productionNext= ["CONST_SYMB","VAR_SYMB", "PROCEDURE_SYMB", "BEGIN_SYMB"]) == False:
+                return
+        
+    
+    def prod_dc_v(self):
+        if self.isCurrentToken("VAR_SYMB"):
+            self.loadNextSymbol()
+        else:
+            if self.error("var esperado.", currentNext= "IDENTIFIER", productionNext= ["VAR_SYMB","PROCEDURE_SYMB", "BEGIN_SYMB"]) == False:
+                self.errors.pop()
+                return
+
+        #<variaveis>
+        while True:
+            if self.isCurrentToken("IDENTIFIER"):
+                self.loadNextSymbol()
+            else:
+                if self.error("Identificador da variável esperado.", currentNext= ["COMMA_SYMB", "COLON_SYMB"], productionNext= ["VAR_SYMB","PROCEDURE_SYMB", "BEGIN_SYMB"]) == False:
+                    return
+
+            if self.isCurrentToken("COMMA_SYMB"):
+                self.loadNextSymbol()
+            else:
+                break
+            
+        if self.isCurrentToken("COLON_SYMB"):
+            self.loadNextSymbol()
+        else:
+            if self.error("Dois pontos esperado.", currentNext= "TYPE_SYMB", productionNext= ["VAR_SYMB","PROCEDURE_SYMB", "BEGIN_SYMB"]) == False:
+                return
+        
+        if self.isCurrentToken("TYPE_SYMB"):
+            self.loadNextSymbol()
+        else:
+            if self.error("Tipo esperado.", currentNext= "SEMICOLON_SYMB", productionNext= ["VAR_SYMB","PROCEDURE_SYMB", "BEGIN_SYMB"]) == False:
+                return
+        
+        if self.isCurrentToken("SEMICOLON_SYMB"):
+            self.loadNextSymbol()
+        else:
+            if self.error("; esperado.", currentNext= ["VAR_SYMB","PROCEDURE_SYMB", "BEGIN_SYMB"], productionNext= ["VAR_SYMB","PROCEDURE_SYMB", "BEGIN_SYMB"]) == False:
+                return
+
+    def prod_dc_p(self):
+        pass
